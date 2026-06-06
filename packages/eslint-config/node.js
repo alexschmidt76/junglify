8e339d74b4177ce6267bb125b9bd235246dbf026
alex <alex.schmidt767@gmail.com>
2026-06-06 05:44:02 -0500

    fixing ts and es configs

---
+++ packages/eslint-config/node.js
@@ -0,0 +1,14 @@
+import globals from "globals";
+import { config as baseConfig } from "./base.js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export const config = [
+  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
