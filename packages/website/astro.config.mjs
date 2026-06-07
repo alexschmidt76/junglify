f0b653a35b4fa277a97d50cdc2393ee09dca880e
alex <alex.schmidt767@gmail.com>
2026-06-07 16:59:33 -0500

    added test url to allowed vite hosts

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        preview: {
+            allowedHosts: [
+                'website-test-production-9009.up.railway.app',
+                'junglify.org'
+            ]
+        },
+    },
+});
