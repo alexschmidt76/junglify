058d349ad233167a52bfccd63f8c8ef8ff27f5f0
alex <alex.schmidt767@gmail.com>
2026-06-07 18:02:50 -0500

    fixing pool connection for auth

--- packages/backend/src/routes/auth.route.ts
+++ packages/backend/src/routes/auth.route.ts
@@ -3,13 +3,9 @@ import PostgresAdapter from "@auth/pg-adapter";
 import { Pool } from "pg";
  
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DB_POOLER_URL,
+  max: 1,
+  ssl: { rejectUnauthorized: false },
 });
  
 const auth = ExpressAuth({
