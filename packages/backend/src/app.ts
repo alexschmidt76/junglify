2466d53aa2a6351d7de95e4c62f7c76868a71c4d
alex <alex.schmidt767@gmail.com>
2026-06-07 13:29:48 -0500

    app using health route

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import auth from './routes/auth.route.js';
+import health from './routes/health.route.js';
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.set('trust proxy', true);
 
 app.use('/auth/*path', auth);
+app.use('/health', health);
 
 export default app;
