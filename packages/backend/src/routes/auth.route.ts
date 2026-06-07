32681b0a1b9bf1bd96de390954df9dc79726b250
alex <alex.schmidt767@gmail.com>
2026-06-07 17:59:45 -0500

    fixing env vars

--- packages/backend/src/routes/auth.route.ts
+++ packages/backend/src/routes/auth.route.ts
@@ -3,10 +3,10 @@ import PostgresAdapter from "@auth/pg-adapter";
 import { Pool } from "pg";
  
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
