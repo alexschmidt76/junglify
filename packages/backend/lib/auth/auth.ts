0c8bd5a8a73859c5f2265bb638d0e41537436b57
alex <alex.schmidt767@gmail.com>
2026-06-08 22:32:22 -0500

    added client auth

--- packages/backend/lib/auth/auth.ts
+++ packages/backend/lib/auth/auth.ts
@@ -1,4 +1,21 @@
 import { betterAuth } from 'better-auth';
+import { username } from 'better-auth/plugins';
 import sql from '../db/sql.js';
 
-export const auth = betterAuth({ database: sql });
+const trustedOrigins: string[] = [];
+if (process.env.EXTENSION_ID) trustedOrigins.push(process.env.EXTENSION_ID);
+if (process.env.FRONTEND_URL) trustedOrigins.push(process.env.FRONTEND_URL);
+
+export const auth = betterAuth({
+  database: {
+    db: sql,
+    type: 'postgresql'
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    username()
+  ],
+  trustedOrigins: trustedOrigins
+});
