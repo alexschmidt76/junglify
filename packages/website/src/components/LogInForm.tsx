1753f80fdfb623e544e27e9b6459526adb64f493
alex <alex.schmidt767@gmail.com>
2026-06-09 00:53:33 -0500

    log in form created

---
+++ packages/website/src/components/LogInForm.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import authClient from '../auth/auth-client';
+
+export default function LogInForm() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const response = 
+            name.includes('@')
+            ? await authClient.signIn.email({ email: name, password: password })
+            : await authClient.signIn.username({ username: name, password: password });
+
+        if (response.error) {
+            setError(response.error.message ?? "Something went wrong");
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Email/Username</label>
+            <input value={name} onChange={e => setName(e.target.value)} required />
+            <label>Password</label>
+            <input value={password} onChange={e => setPassword(e.target.value)} type='password' required />
+            { error && <p>{error}</p>}
+            <button type='submit'>Log In</button>
+        </form>
+    )
+}
