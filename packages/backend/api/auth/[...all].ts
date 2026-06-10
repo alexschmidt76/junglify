b60897a76a5045db5c7b125f029613b8b160f2ea
alex <alex.schmidt767@gmail.com>
2026-06-09 20:33:59 -0500

    finished creating changelongs and setting versions

--- packages/backend/api/auth/[...all].ts
+++
@@ -1,4 +0,0 @@
-import { toNextJsHandler } from 'better-auth/next-js';
-import auth from '@/lib/auth/auth.js';
-
-export const { POST, GET } = toNextJsHandler(auth);
--- packages/backend/db/migrations/002_add_username_columns.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" ADD COLUMN "username" TEXT UNIQUE;
-ALTER TABLE "user" ADD COLUMN "display_name" TEXT;
