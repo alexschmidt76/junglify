ad8d47122c44b7f05cea2a5c02c57bbf57d42c4e
alex <alex.schmidt767@gmail.com>
2026-06-13 00:05:02 -0500

    login and log out forms styled

--- packages/react-components/auth-forms/LogInForm.tsx
+++ packages/react-components/auth-forms/LogInForm.tsx
@@ -20,13 +20,23 @@ export default function LogInForm({ authClient }: { authClient: JungleAuthClient
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Email/Username</label>
-            <input value={name} onChange={e => setName(e.target.value)} required />
-            <label>Password</label>
-            <input value={password} onChange={e => setPassword(e.target.value)} type='password' required />
-            { error && <p>{error}</p>}
-            <button type='submit'>Log In</button>
-        </form>
+        <div className='flex mx-auto w-min p-4 bg-black text-xl text-green-600 rounded-2xl'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+                <h1 className='text-green-500 text-2xl font-bold mx-auto'>Log In to Junglify</h1>
+                <p className='text-white/50 text-lg mx-auto'>New to Junglify? <a href='/sign-up' className='text-green-800 hover:font-bold'>Sign Up Here</a></p>
+                <div className='flex flex-col mx-auto [&_input]:bg-white/80 [&_input]:rounded-sm'>
+                    <div className='flex flex-col mx-auto'>
+                        <label>Email/Username</label>
+                        <input value={name} onChange={e => setName(e.target.value)} required />
+                    </div>
+                    <div className='flex flex-col mx-auto'>
+                        <label>Password</label>
+                        <input value={password} onChange={e => setPassword(e.target.value)} type='password' required />
+                    </div>
+                    { error && <p>{error}</p>}
+                </div>
+                <button type='submit' className='bg-green-600 text-black font-semibold rounded-lg px-2 pt-0.5 pb-1 mx-auto hover:bg-green-500 hover:cursor-pointer'>Log In</button>
+            </form>
+        </div>
     )
 }
