6af87665f3f8e0c3de59fe4fc653b1dc352aee6a
alex <alex.schmidt767@gmail.com>
2026-06-13 04:43:06 -0500

    small update

--- apps/api/lib/db/sql.ts
+++ apps/api/lib/db/sql.ts
@@ -4,7 +4,7 @@ import isDev from '@repo/utils/isDev';
 const url = process.env[isDev() ? 'DEV_DATABASE_URL' : 'DATABASE_URL'];
 
 if (!url) {
-  throw new Error('DB_POOLER_URL environment variable is not set');
+  throw new Error('DATABASE_URL env var is not set');
 }
 
 const sql = postgres(url, { max: 1});
