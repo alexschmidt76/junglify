f767e067f0c5f0576eedad535d18e60967fcf897
alex <alex.schmidt767@gmail.com>
2026-06-15 22:20:11 -0500

    added update user service

--- apps/api/lib/services/user.service.ts
+++ apps/api/lib/services/user.service.ts
@@ -1,6 +1,8 @@
 import sql from '../db/sql.js';
 
-export const getPopupInfo = async (userId) => {
+import type User from '@repo/utils/types/user';
+
+export const getPopupInfo = async (userId: string) => {
     const [stash] = await sql`
         SELECT j.url, s.banana_count
         FROM stashes s
@@ -15,4 +17,20 @@ export const getPopupInfo = async (userId) => {
     `;
     
     return { stash, jungles };
+}
+
+export const updateUser = async (updateUser: User) => {
+    const { id, ...updateFields } = updateUser;
+
+    if (!id) throw new Error('id is required');
+
+    const updates = { updatedAt: Date.now(), ...updateFields };
+
+    const { count } = await sql`
+        UPDATE user
+        SET ${sql(updates)}
+        WHERE id = ${id};
+    `;
+
+    return count > 0;
 }
